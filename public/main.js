2104e063ba76c505f6c296f59780de060b986feb
Laysa Souza <karollinylaysa@gmail.com>
2023-05-23 12:10:12 +0100

    Added notification  feature from Electron

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 // main.js
 // Modules to control application life and create native browser window
-const {app, BrowserWindow } = require('electron')
+const {app, BrowserWindow, Notification } = require('electron')
+const {join} = require("path");
 
 const createWindow = () => {
     // Create the browser window.
@@ -11,26 +12,30 @@ const createWindow = () => {
             enableRemoteModule: true,
         }
     })
+
     // and load the localhost
     mainWindow.loadURL('http://localhost:3000')
+}
+
+function showNotification() {
+    new Notification({ title: 'Welcome to HAVS Calculator', body: 'Enter your tools and times below to find out the vibration exposure' }).show()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-    createWindow()
-    app.on('activate', () => {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+app.whenReady().then(createWindow).then(showNotification);
 
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
+
     if (process.platform !== 'darwin') app.quit()
 })
--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
 import './App.css';
+import styled, {ThemeProvider} from "styled-components";
 import {ResultComponent} from "./components/results/ResultComponent";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SunIcon from "./components/icons/SunIcon";
-import MoonIcon from "./components/icons/MoonIcon";
 import Switch from "./components/switch/Switch.js";
 
+const darkTheme = {
+    body: '#1c1c1c',
+    title: '#ffff',
+}
+
+const lightTheme = {
+    body: '#fff',
+    title: '#6200c5',
+}
+
+const StyledApp = styled.div`
+  text-align: center;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 14px;
+  background-color: #ffff ;
+`
+const AppName = styled.h1`
+  color: ${(props) => props.theme.title}
+`
 function App() {
 
     return (
-            <div className="App m-4">
+        <ThemeProvider theme={lightTheme}>
+            <StyledApp className="App m-4">
                 <div className="mt-5">
-                    <img src={"./logo.png"} alt="logo"/>
+                    <div className="row">
+                        <div className="col">
+                            <img src={"./logo.png"} alt="logo"/>
+                        </div>
+                        <div className="col-2">
+                            <Switch/>
+                        </div>
+                    </div>
                 </div>
                 <div className="intro-info">
-                    <h1>HAVS Calculator</h1>
-                    <p className="m-4">Are you wondering how to calculate vibration exposure, for one tool or multiple
-                        tools, and stay
-                        within the legal
-                        limits? Enter your tools and times below to find out. The HAVS calculator will tell you if you are
-                        under or over
-                        the exposure action value, and how close you are to the limit.</p>
+                    <AppName color={"#6200c5"}>HAVS Calculator</AppName>
+                    <p className="m-4">Are you wondering how to calculate vibration exposure, for one tool and stay
+                        within the legal limits?
+                        Enter your tools and times below to find out. The HAVS calculator will tell you if you are
+                        under or over the exposure action value, and how close you are to the limit.</p>
                 </div>
                 <ResultComponent/>
-                <SunIcon/>
-                <Switch />
-                <MoonIcon />
-            </div>
+            </StyledApp>
+        </ThemeProvider>
     );
 }
 
